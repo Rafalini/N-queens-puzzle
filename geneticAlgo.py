e0f55ed54f0a508eb8e4c290e64c6d4dd79816bf
kacpi <zkacper03@gmail.com>
2021-04-06 21:21:30 +0200

    v1.1, crossover implemented

--- geneticAlgo.py
+++ geneticAlgo.py
@@ -1,92 +1,144 @@
 import random
 import copy
 import numpy as np
+from operator import attrgetter
 
-#tournamentSize = 2 #encoded
+# tournamentSize = 2 #encoded
 random.seed(0)
 
-class Quen:
+
+# seed do zmiany
+
+class Queen:
     def __init__(self, x, y):
-        self.x=x
-        self.y=y
+        self.x = x
+        self.y = y
 
     def __eq__(self, another):
-        if not isinstance(another, Quen):
+        if not isinstance(another, Queen):
             return NotImplemented
         return self.x == another.x and self.y == another.y
 
 
 class Board:
-    def __init__(self, n):
-        #unique field numbers:
-        self.occupiedFields=set()
-        self.quenMembers = []
-        quenPositions = random.sample(range(0,n*n-1),n) # !!! n>1 !!! or exception will occur
-        i=0
-        for nr in quenPositions:            #numbers go letf to right, top to bottom
-            # self.quenMembers.append(Quen(nr%n,int(nr/n)))
-            # self.occupiedFields.add(nr)
-            self.quenMembers.append(Quen(i,0))
-            self.occupiedFields.add(i)
-            i+=1
+    def __init__(self, n, ifNeedsPopulating):
+        # unique field numbers:
+        self.occupiedFields = set()
+        self.queenMembers = []
+        if (ifNeedsPopulating):
+            self.populate(n)
+        else:
+            self.queenMembers = [0 for x in range(n)]
+        self.updateLossFunction()
+
+    def populate(self, n):
+        queenPositions = random.sample(range(0, n * n - 1), n)  # !!! n>1 !!! or exception will occur
+        for nr in queenPositions:  # numbers go left to right, top to bottom
+            self.queenMembers.append(Queen(nr % n, int(nr / n)))
+            self.occupiedFields.add(nr)
         # Kth row, Nth collumn, field_nr = n*row + collumn
         # row = field_nr/n   collumn = field_nr%n
-        self.updateLossFunction()
 
     def updateLossFunction(self):
         checks = 0
-        for quen1 in self.quenMembers:
-            for quen2 in self.quenMembers:
-                if quen1 != quen2:
-                    if quen1.x == quen2.x: #row
+
+        # do optymalizacji
+        for queen1 in self.queenMembers:
+            for queen2 in self.queenMembers:
+                if queen1 != queen2:
+                    if queen1.x == queen2.x:  # row
                         checks += 1
-                    if quen1.y == quen2.y: #collumn
+                    if queen1.y == queen2.y:  # collumn
                         checks += 1
-                    if quen1.x - quen1.y == quen2.x - quen2.y: #diagonal
+                    if queen1.x - queen1.y == queen2.x - queen2.y:  # diagonal
                         checks += 1
-                    if - quen1.y - quen1.x == - quen2.x - quen2.y: #-diagonal
+                    if - queen1.y - queen1.x == - queen2.x - queen2.y:  # -diagonal
                         checks += 1
         self.fitness = checks
 
     def printBoard(self):
-        print("Fitness: "+str(self.fitness))
-        for quen in self.quenMembers:
-             print("Quen, ox: "+str(quen.x)+" oy: "+str(quen.y))
+        print("Fitness: " + str(self.fitness))
+        for queen in self.queenMembers:
+            print("Queen, ox: " + str(queen.x) + " oy: " + str(queen.y))
+
+    def printBoard2(self):
+        print('\n')
+        print(self.occupiedFields)
+
+        for x in range(8 * 8):
+            if x % 8 == 0:
+                print()
+            if x in self.occupiedFields:
+                print('| X ', end='')
+            else:
+                print('| _ ', end='')
 
 
 class Population:
-    def __init__(self, populationSize, boardSize, quenMutationProbability=0.02, boardMutationProbability=0.01):
+    def __init__(self, populationSize, boardSize, queenMutationProbability=0.1, boardMutationProbability=0.5):
         self.populationSize = populationSize
         self.boardSize = boardSize
-        self.quenMutationProbability = quenMutationProbability
+        self.queenMutationProbability = queenMutationProbability
         self.boardMutationProbability = boardMutationProbability
-        self.members = [Board(boardSize) for i in range(populationSize)]
-        self.distribution = [1.0/(populationSize * populationSize) * ((populationSize - i) * (populationSize - i) - (populationSize - i - 1) * (populationSize - i - 1)) for i in range(populationSize)]
+        self.members = [Board(boardSize, True) for i in range(populationSize)]
+        self.distribution = [1.0 / (populationSize * populationSize) * (
+                (populationSize - i) * (populationSize - i) - (populationSize - i - 1) * (populationSize - i - 1))
+                             for i in range(populationSize)]
 
     def tournamentSelection(self):
-        bestBoards = []
-        self.members.sort(key=lambda board: board.fitness)
+        children = []
         for i in range(self.populationSize):
-            tournament = np.random.choice(self.members, 2, self.distribution)
-            if tournament[0].fitness > tournament[1].fitness:
-                bestBoards.append(copy.deepcopy(tournament[0]))
-            else:
-                bestBoards.append(copy.deepcopy(tournament[1]))
-        return bestBoards
+            tournament = np.random.choice(self.members, 4, self.distribution)
+            index = random.randint(0, self.boardSize - 1)
+            tournament[0] = min(tournament, key=attrgetter('fitness'))
+            tournament[1] = min(tournament[1], tournament[2], tournament[3], key=attrgetter('fitness'))
+            lengthOfSegm = random.randint(1, self.boardSize - index)
+
+            newBoard = Board(self.boardSize, False)
+            for x in range(lengthOfSegm):
+                newBoard.queenMembers[index + x] = copy.deepcopy(tournament[0].queenMembers[index + x])
+                newBoard.occupiedFields.add(
+                    tournament[0].queenMembers[index + x].y * self.boardSize
+                    + tournament[0].queenMembers[index + x].x)
+
+            newIndexChild = index + lengthOfSegm
+            newIndexParent = index + lengthOfSegm
+            while newIndexChild != index:
+                if (newIndexChild >= self.boardSize):
+                    newIndexChild = 0
+                    continue
+                if (newIndexParent >= self.boardSize):
+                    newIndexParent = 0
+                    continue
+                field = tournament[1].queenMembers[newIndexParent].y * self.boardSize + tournament[1].queenMembers[
+                    newIndexParent].x
+                if (field in newBoard.occupiedFields):
+                    newIndexParent += 1
+                else:
+                    newBoard.queenMembers[newIndexChild] = copy.deepcopy(tournament[1].queenMembers[newIndexParent])
+                    newBoard.occupiedFields.add(
+                        tournament[1].queenMembers[newIndexParent].y * self.boardSize
+                        + tournament[1].queenMembers[newIndexParent].x)
+
+                    newIndexChild += 1
+                    newIndexParent += 1
+            newBoard.updateLossFunction()
+            children.append(newBoard)
+
+        return children
 
     def mutatedBoards(self, bestBoards):
         for board in bestBoards:
-            if board != bestBoards[0] and random.uniform(0, 1) < self.boardMutationProbability:
-                for quen in board.quenMembers:
-                    if random.uniform(0, 1) < self.quenMutationProbability:
-                        field = random.randint(0, self.boardSize*self.boardSize-1)
+            if random.uniform(0, 1) < self.boardMutationProbability:
+                for queen in board.queenMembers:
+                    if random.uniform(0, 1) < self.queenMutationProbability:
+                        field = random.randint(0, self.boardSize * self.boardSize - 1)
                         while field in board.occupiedFields:
-                            field = random.randint(0, self.boardSize*self.boardSize-1)
-
-                        board.occupiedFields.remove(self.boardSize*quen.y+quen.x)
+                            field = random.randint(0, self.boardSize * self.boardSize - 1)
+                        board.occupiedFields.remove(self.boardSize * queen.y + queen.x)
                         board.occupiedFields.add(field)
-                        quen.x = field%self.boardSize
-                        quen.y = int(field/self.boardSize)
+                        queen.x = field % self.boardSize
+                        queen.y = int(field / self.boardSize)
                     board.updateLossFunction()
         return bestBoards
 
@@ -94,13 +146,17 @@ class Population:
         for i in range(epochs):
             bestBoards = self.tournamentSelection()
             mutatedBoards = self.mutatedBoards(bestBoards)
-            self.members += mutatedBoards
-            self.members.sort(key=lambda board: board.fitness)
-            self.members = self.members[0:self.populationSize+1]
-            self.members.sort(key=lambda board: board.fitness)
+            self.members = mutatedBoards
 
-            int = 0
+            sumOfFitness = 0
+            fittest = 1000
+            fittestBoard = ''
             for board in self.members:
-                int += board.fitness
-            avg = int/len(self.members)
-            print("Epoch nr: "+str(i)+" fittest candidate: "+str(self.members[0].fitness)+"  avg.fit: "+"%.2f" % avg)
+                if board.fitness < fittest:
+                    fittest = board.fitness
+                    fittestBoard = board
+                sumOfFitness += board.fitness
+            avgFit = sumOfFitness / len(self.members)
+            fittestBoard.printBoard2()
+            print()
+            print("Epoch nr: " + str(i) + " fittest candidate: " + str(fittest) + "  avg.fit: " + "%.2f" % avgFit)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from geneticAlgo import Quen
-from geneticAlgo import Board
 from geneticAlgo import Population
 
-p1 = Population(1000, 20)
-p1.evolve(1000)
+if __name__ == '__main__':
+    p1 = Population(100, 8, 0.3, 0.4)
+    p1.evolve(1000)
+
